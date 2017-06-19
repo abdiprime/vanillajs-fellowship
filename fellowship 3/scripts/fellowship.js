a8a4bc38c1770b8492a55efa46ffdc22a5477371
Abdiqani Abdulkarim <AbdiqaniAbdulkarim@Halimos-MacBook-Pro.local>
2017-06-19 13:37:56 -0500

    jqueryfellowship

--- fellowship 3/scripts/fellowship.js
+++ fellowship 3/scripts/fellowship.js
@@ -1,111 +1,204 @@
-    makeMiddleEarth: function(lands){
+console.log("Linked.");
 
-     document.createElement('section').setAttribute('id','middle-earth');
-     for (var i = 0; i<fellowship.lands.length; i++){
-         var article = document.createElement('article');
-         document.querySelector('#middle-earth').appendChild(article);
-         var h1 = document.createElement('h1');
-         h1.textContent = fellowship.lands[i];
-         article.appendChild(h1);
-       }
-    }		    }
-    makeHobbits: function(hobbits){
+// Dramatis Personae
+var hobbits = [
+  'Frodo Baggins',
+  'Samwise \'Sam\' Gamgee',
+  'Meriadoc \'Merry\' Brandybuck',
+  'Peregrin \'Pippin\' Took'
+];
 
-     var target = document.querySelector('#middle-earth').firstChild.nextSibling;
-     var list = document.createElement('ul');
-     target.appendChild(list);
-     for(var i=0; i<fellowship.hobbits.length; i++){
-       var item = document.createElement('li');
-       item.setAttribute('class','hobbit');
-       item.textContent = fellowship.hobbits[i];
-       list.appendChild(item);
-     }
-    }		    
-    keepItSecretKeepItSafe: function(){
-     
-     var frodo = document.querySelector('ul').firstChild;
-     var ring = document.createElement('div');
-     ring.setAttribute('id','the-ring');
-     frodo.appendChild(ring);
- 
-    }		  
-    makeBuddies: function(buddies){
-  
-     var aside = document.createElement('aside');
-     var list = document.createElement('ul');
-     var lands = document.querySelector('#middle-earth');
-     var rivendell = document.querySelector('#middle-earth').lastChild.previousSibling;
-     aside.appendChild(list);
-     lands.insertBefore(aside, rivendell);
-     for(var i=0; i<fellowship.buddies.length; i++){
-       var item = document.createElement('li');
-       item.textContent = fellowship.buddies[i];
-       list.appendChild(item);
-     }
-    }		    
-    beautifulStranger: function(){
-  
-     var strider = document.querySelector('aside').firstChild.lastChild.previousSibling;
-     strider.innerHTML = "Aragorn";
-    }
-    
-    forgeTheFellowShip: function(){
-    
-     var article = document.querySelector('#middle-earth').firstChild.nextSibling;
-     var oldHobbits = document.querySelector('#middle-earth').firstChild.nextSibling.firstChild;
-     var aside = document.querySelector('aside');
-     var oldBuddies = document.querySelector('aside').firstChild;
-     var hobbits = article.removeChild(oldHobbits);
-     var buddies = aside.removeChild(oldBuddies);
-     var rivendell = document.querySelector('#middle-earth').lastChild.previousSibling;
-     rivendell.appendChild(hobbits);
-     rivendell.appendChild(buddies);
-     var theFellowship = document.createElement('div');
-     for (var i=0; i<fellowship.hobbits.length; i++){
-       theFellowship.textContent += fellowship.hobbits[i];
-       alert(fellowship.hobbits[i]+" has joined the fellowship!");
-     }
-     for (var i=0; i<fellowship.buddies.length; i++){
-       theFellowship.textContent += fellowship.buddies[i];
-       alert(fellowship.buddies[i]+" has joined the fellowship!");
-     }
- 
-    }	    
+var buddies = [
+  'Gandalf the Grey',
+  'Legolas',
+  'Gimli',
+  'Aragorn',
+  'Boromir'
+];
+
+var lands = ['The Shire', 'Rivendell', 'Mordor'];
+var body = document.body;  
+
+var makeMiddleEarth = function(lands) {
+   // create a section tag with an id of `middle-earth`
+  var section =  document.createElement('section')
+   section.id = 'middleEarth'
+
+ 	 // add each land as an `article` tag
+   for(i = 0; i < lands.length; ++i){
+	var article = document.createElement('article')
+   	var h1 = document.createElement('h1')
+    article.appendChild(h1);
+    h1.innerHTML = [i];	
+   	section.append(article)
+}
 
 
 
 
 
 
+var lands = 
+
+  	 // loop through each land, add the text of the land, and append to the page
+   // inside each `article` tag include an `h1` with the name of the land
+   // append `middle-earth` to your document `body`
+   body.append(section)
+};
+
+makeMiddleEarth(lands);
+
+
+
+var makeHobbits = function () {
+	var ul = document.createElement('ul')
+	for(i = 0; i < hobbits.length; i++){
+		var li = document.createElement('li')
+		li.className = 'hobbit'
+		li.innerHTML = hobbits[i]
+		ul.appendChild(li)
+
+	}
+	var shire = document.getElementsByTagName('article')[0]
+	shire.appendChild(ul)
+}
+
+makeHobbits()
+
+// display an `unordered list` of hobbits in the shire
+      // (which is the second article tag on the page)
+   // give each hobbit a class of `hobbit`
+
+
+var keepItSecretKeepItSafe = function () {
+   // create a div with an id of `'the-ring'`
+   var div = document.createElement('div')
+  div.id = 'theRing'
+  div.className = 'magic-imbued-jewelry'
+  var Frodo = document.getElementsByTagName('li')[0]
+  Frodo.appendChild(div)
+   // give the div a class of `'magic-imbued-jewelry'
+}
+
+keepItSecretKeepItSafe()
+
+
+var makeBuddies = function () {
+	var aside = document.createElement('aside')
+	var ul = document.createElement('ul')
+	aside.appendChild(ul)
+	var rivendell = document.getElementsByTagName('article')[1]
+	rivendell.appendChild(aside)
+	for(i = 0; i < buddies.length; i++){
+		var li = document.createElement('li')
+		ul.appendChild(li)
+		li.innerHTML = buddies[i];
+	}
+   // create an `aside` tag
+   // attach an `unordered list` of the `'buddies'` in the aside
+   // insert your aside as a child element of `rivendell`
+}
+makeBuddies()
+
+var leaveTheShire = function () {
+	// Hobbies<List<String>> result = new ArrayList<List<String>>();
+	// rivendell.appendChild(hobbit)
+
+   // assemble the `hobbits` and move them to `rivendell`
+};
+
+
+var forgeTheFellowShip = function () {
+   // create a new div called `'the-fellowship'` within `rivendell`
+   // add each `hobbit` and `buddy` one at a time to `'the-fellowship'`
+   // after each character is added make an alert that they // have joined your party
+
+var div = document.createElement('div')
+	div.id= 'the-fellowship'
+	var rivendell = document.getElementsByTagName('article')[1]
+	rivendell.appendChild(div)
+	var ul = document.createElement('ul')
+var hobbitsAndBuddies = document.querySelectorAll('li')
+console.log(hobbitsAndBuddies.length)
+	for(i = 0; i < hobbitsAndBuddies.length; i++){
+		ul.appendChild(hobbitsAndBuddies[i])
+	}
+  	
+  	div.appendChild(ul)
+};
+
+forgeTheFellowShip()
+
+var theBalrog = function () {
+   // change the `'Gandalf'` text to `'Gandalf the White'`
+   // apply the following style to the element, make the // background 'white', add a grey border
+	var list = document.getElementsByTagName('ul')[2];
+	list.getElementsByTagName('li')[4].innerHTML = "Gandalf the White"
+	list.getElementsByTagName('li')[4].style.backgroundColor = "white"
+	list.getElementsByTagName('li')[4].style.border = "thick solid grey"	
+};
+
+theBalrog()
+
+
+
+var hornOfGondor = function () {
+   // pop up an alert that the horn of gondor has been blown
+   // Boromir's been killed by the Uruk-hai!
+   // Remove `Boromir` from the Fellowship
+
+	alert('The horn of gondor has been blown')
+	var list = document.getElementsByTagName('ul')[2];
+	 var Boromir = document.getElementsByTagName('li')[8] 
+	list.removeChild(Boromir)
+		
+	
+
+
+};
+
+hornOfGondor()
 
 
 
 
+var itsDangerousToGoAlone = function (){
+   // take `Frodo` and `Sam` out of the fellowship and move // them to `Mordor`
+   // add a div with an id of `'mount-doom'` to `Mordor`
+
+var Frodo = document.getElementsByTagName('li')[0]
+var Sam = document.getElementsByTagName('li')[1]
+var Mordor = document.getElementsByTagName('article')[2]
+var ul = document.createElement('ul')
+	ul.appendChild(Sam)
+	ul.appendChild(Frodo)
+Mordor.appendChild(ul)
+var div = document.createElement('div')
+div.id = "mount-doom"
+
+};
+
+itsDangerousToGoAlone()
 
 
 
+var thereAndBackAgain = function(){
+var list = document.getElementsByTagName('ul')[2];
+var shire = document.getElementsByTagName('article')[0]
+var hobbits = document.getElementsByTagName('li')
+	for(i = 0; i < hobbits; i++){
+		shire.append(hobbits[i])
+	}
 
 
 
+theRing.remove('li')
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+thereAndBackAgain()
+   // remove `Gollum` and `the Ring` from the document
+   // Move all the `hobbits` back to `the shire`
 
 
 
---
+++ fellowship 3/scripts/homeworkcatchup.js
@@ -0,0 +1,70 @@
+// // write a function that takes a couple of words 
+// and counts the letters in each word and returns the word as a number
+
+
+var names = ["kobe", "reggie", "MJ", "Tracy"]
+
+function lengths(morenames){
+	console.log(morenames)
+
+}
+
+
+// take 3 arguments and multiply the first two and then take the 
+// product from that and take it to the power of the last argument.
+var transmogrifier = function(me,myself,I){
+	console.log(me * myself)
+	var p = me * myself 
+	return Math.pow(p,I)
+
+
+}
+
+// write a function called toonify that takes 2 words (accent, sentence)
+// and 
+
+var toonify = function(accent, sentence){
+	if (accent === "daffy"){
+	return sentence.replace(/s/g ,"th")
+}
+}
+
+
+// write function [that takes each letter in the array splits the word up and then fips the lasst letter to the beginning of the sentence]
+
+function wordreverse(sentence){
+ array.rev(sentence)
+}
+
+function letterReverse(word){
+ array.rev(word)
+}
+
+
+
+
+
+// write a  function that reads a list of words and figures out which 
+// word is the longest.
+
+
+
+var word = ""
+   
+var longestWord = function(words){
+	words = ["Chicago", "Minneapolis", "Massachusetts", "Fargo"]
+	for(i = 0; i < longestWord.length; i++){
+		if 
+	}
+}
+
+// write a function that accepts 2 arguments (input,function) 
+// input should somehow be used with function the function should 
+// retun a string and the answer you get from that should result in 
+// "proves that  I am the rep MASTER"
+
+function repMaster(input, function)
+
+
+
+
